da710f9e628f7e5603f683228a9e53bff81f88c0
Manuel Moya <moya.manuel41@gmail.com>
2018-11-18 04:31:07 -0800

    Implement home page styles and views

--- root-folder/CRUD/views.py
+++ root-folder/CRUD/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'CRUD/index.html')
 
 def test(request):
     return HttpResponse("test!")
